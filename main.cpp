6da25803975ce4e513327995a1fe482d015dc640
Fernando Castro Gadelha <fernandinhocastrogadelha@gmail.com>
2026-04-27 06:00:10 -0300

    Base do boneco feita

--- main.cpp
+++ main.cpp
@@ -5,10 +5,26 @@
 
 
 int main() {
-    Sculptor draw(50,50,50);
+    Sculptor draw(75,75,75);
 
-    draw.setColor(1.0, 0.0, 0.0, 1.0);
+    //torso
+    draw.setColor(0.0, 0.0, 1.0, 1.0);
     draw.putBox(15, 35, 20, 40, 20, 30);
+
+    //braços
+    draw.setColor(1.0, 1.0, 0.0, 1.0);
+    draw.putBox(36, 45, 20, 40, 20, 30);
+    draw.putBox(5, 14, 20, 40, 20, 30);
+
+    //pernas
+    draw.setColor(0.0, 1.0, 0.0, 1.0);
+    draw.putBox(15, 25, 0, 19, 20, 30);
+    draw.putBox(16, 35, 0, 19, 20, 30);
+
+    //cabeça
+    draw.setColor(1.0, 1.0, 0.0, 1.0);
+    draw.putEllipsoid(25, 50, 25, 8, 10, 8);
+
 
     draw.writeOFF("Roblox.off");
 
